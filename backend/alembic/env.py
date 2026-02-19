acc92dde22d0851a07881623b3d2dc84ad8a3e5e
Md Nazmul Bijoy <nazmulbijoy9105@gmail.com>
2026-02-19 16:35:26 +0600

    fix: correct alembic env.py import path

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -13,7 +13,7 @@ config.set_main_option("sqlalchemy.url", os.environ.get("DATABASE_URL", ""))
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-from app.database import Base
+from backend.app.database import Base
 from app.models import Question, Response
 
 target_metadata = Base.metadata
